8e3c0eca20d93fa67203cede2edcca6d03c5333e
minjae1025 <kjt081025@gmail.com>
2026-06-07 13:14:50 +0900

    궁합 DB 저장 및 EMAIL 전송 수정

--- src/transmission/dto/create-transmission.dto.ts
+++ src/transmission/dto/create-transmission.dto.ts
@@ -1,9 +1,12 @@
 import { IsEmail, IsString, IsUUID } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
 
 export class CreateTransmissionDto {
+  @ApiProperty({ description: '사용자 UUID', example: 'a96dd080-5d4c-11f1-9ef4-c39965e34bd0' })
   @IsUUID()
-  userId: string;
+  userId!: string;
 
+  @ApiProperty({ description: '사용자 이메일', example: 'user@example.com' })
   @IsEmail()
-  email: string;
+  email!: string;
 }
